32be75e9bbcd07b7b37dc1409fcdbc65b629a7af
K Sheenan <kaynesheenan+git@gmail.com>
2021-07-23 00:05:17 +0900

    game

--- app.js
+++ app.js
@@ -52,3 +52,60 @@ document.addEventListener('DOMContentLoaded', () => {
 
     ]
 })
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
+var cardsChosen = []
+var cardsChosenId = []
+var cardsWon = []
+
+// create a board
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        // card.addEventListener('click', flipcard)
+        grid.appendChild(card)
+    }
+}
+
+// check for matches
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] ===cardsChosen[1]) {
+        alert('you found a match!')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cardsWon.push(cardsChosen)
+    } else  {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = 'Congratulations, you found them all!'
+    }
+}
+
+
+// flip card
+function flipCard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2){
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+
+createBoard()
